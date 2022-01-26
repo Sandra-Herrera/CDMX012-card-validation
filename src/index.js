e95f093b9a8f0f194649fb2bae9207cf3f004053
Sandra-Herrera <97549362+Sandra-Herrera@users.noreply.github.com>
2022-01-26 03:08:42 -0600

    modal e integracion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import validator from './validator.js';
 
-console.log(validator.validateCreditCard('4213456789542013'));
+export function validar(){
+    let creditCardNumber = window.document.getElementById('input-numero').value;
+    creditCardNumber = creditCardNumber.replaceAll("-","");
+    let validate = validator.validateCreditCard(creditCardNumber);
+    mostrarModal(validate);
+}
+
+function mostrarModal(validate){
+    let modal = document.getElementById("myModal");
+    let modalContainer = document.getElementById("modalContainer");
+    let message = document.getElementById("messageModal");
+    modal.style.display = "block";
+    if(validate){
+        modalContainer.style.backgroundColor = "#6AA84F";
+        message.innerHTML = "Tarjeta de credito valida";        
+    }else{
+        modalContainer.style.backgroundColor = "red"
+        message.innerHTML = "Tarjeta de credito invalida";
+    }
+}
+export function closeModal(){
+    let modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+
+window.validar = validar;
+window.closeModal = closeModal;
--- src/validator.js
+++ src/validator.js
@@ -1,12 +1,12 @@
 const validator = {
-  validateCreditCard: function(creditCardNumber){ //"4213456789542013"
-    let arrayCreditCardNumber = Array.from(creditCardNumber); // arrayCreditCardNumber --> ['4', '2', '1', '3', '4', '5', '6', '7', '8', '9', '5', '4', '2', '0', '1', '3']
+  validateCreditCard: function(creditCardNumber){ //"4213456789542015"
+    let arrayCreditCardNumber = Array.from(creditCardNumber); // arrayCreditCardNumber --> ['4', '2', '1', '3', '4', '5', '6', '7', '8', '9', '5', '4', '2', '0', '1', '5']
 
     for (let index = 0; index < arrayCreditCardNumber.length; index++) {
       arrayCreditCardNumber[index] = parseInt(arrayCreditCardNumber[index]);
-    } // arrayCreditCardNumber --> [4, 2, 1, 3, 4, 5, 6, 7, 8, 9, 5, 4, 2, 0, 1, 3]
+    } // arrayCreditCardNumber --> [4, 2, 1, 3, 4, 5, 6, 7, 8, 9, 5, 4, 2, 0, 1, 5]
 
-    arrayCreditCardNumber.reverse(); // arrayCreditCardNumber --> [3, 1, 0, 2, 4, 5, 9, 8, 7, 6, 5, 4, 3, 1, 2, 4]
+    arrayCreditCardNumber.reverse(); // arrayCreditCardNumber --> [5, 1, 0, 2, 4, 5, 9, 8, 7, 6, 5, 4, 3, 1, 2, 4]
   
     for (let index = 1; index < arrayCreditCardNumber.length; index = index + 2) {
       arrayCreditCardNumber[index] = arrayCreditCardNumber[index] * 2;
@@ -26,7 +26,7 @@ const validator = {
       sumTotal = sumTotal + arrayCreditCardNumber[index];
     }
 
-    if(sumTotal%10 === 0){
+    if(sumTotal > 0 && sumTotal%10 === 0){
       return true;
     }
     else{
